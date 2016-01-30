8f46490d89460ea1575f7496a68f1589a8e0259c
johnmiller86 <chefjohn2006@hotmail.com>
2016-01-29 21:20:50 -0500

    1.0.2

--- app/src/main/java/com/ist402/jdm5908/burgercaloriecounter/MainActivity.java
+++ app/src/main/java/com/ist402/jdm5908/burgercaloriecounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.ist402.jdm5908.burgercaloriecounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
@@ -42,5 +43,60 @@ public class MainActivity extends AppCompatActivity {
         sauceSBR.setOnSeekBarChangeListener(sauceListener);
     }
 
+    private RadioGroup.OnCheckedChangeListener foodListener = new RadioGroup.OnCheckedChangeListener() {
 
+        @Override
+        public void onCheckedChanged(RadioGroup rbGroup, int radioId) {
+            switch (radioId) {
+                case R.id.radio0:
+                    burger.setPattyCalories(Burger.BEEF);
+                    break;
+                case R.id.radio1:
+                    burger.setPattyCalories(Burger.LAMB);
+                    break;
+                case R.id.radio2:
+                    burger.setPattyCalories(Burger.OSTRICH);
+                    break;
+                case R.id.radio3:
+                    burger.setCheeseCalories(Burger.ASIAGO);
+                    break;
+                case R.id.radio4:
+                    burger.setCheeseCalories(Burger.CREME_FRAICHE);
+            }
+            displayCalories();
+        }
+    };
+
+    private CheckBox.OnClickListener baconListener = new CheckBox.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if (((CheckBox) v).isChecked())
+                burger.setProsciuttoCalories(Burger.PROSCIUTTO);
+            else
+                burger.clearProsciuttoCalories();
+            displayCalories();
+        }
+    };
+
+    private SeekBar.OnSeekBarChangeListener sauceListener = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            burger.setSauceCalories(seekBar.getProgress());
+            displayCalories();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+    };
+
+    private void displayCalories() {
+        String calorieText = "Calories: " + burger.getTotalCalories();
+        caloriesTV.setText(calorieText);
+    }
 }
