0c54fb82a69a0f137ecd1c2774be2ea08306325d
johnmiller86 <chefjohn2006@hotmail.com>
2016-01-29 20:41:30 -0500

    Burger Project

--- app/src/main/java/com/ist402/jdm5908/burgercaloriecounter/MainActivity.java
+++ app/src/main/java/com/ist402/jdm5908/burgercaloriecounter/MainActivity.java
@@ -1,13 +1,46 @@
 package com.ist402.jdm5908.burgercaloriecounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup pattyRG;
+    private CheckBox prosciuttoCBX;
+    private RadioGroup cheeseRG;
+    private SeekBar sauceSBR;
+    private TextView caloriesTV;
+    private Burger burger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        burger = new Burger();
+        initialize();
+        registerChangeListener();
     }
+
+    private void initialize() {
+        pattyRG = (RadioGroup) findViewById(R.id.radioGroup1);
+        prosciuttoCBX = (CheckBox) findViewById(R.id.checkBox1);
+        cheeseRG = (RadioGroup) findViewById(R.id.radioGroup2);
+        sauceSBR = (SeekBar) findViewById(R.id.seekBar1);
+        caloriesTV = (TextView) findViewById(R.id.textView2);
+
+        displayCalories();
+    }
+
+    private void registerChangeListener() {
+        pattyRG.setOnCheckedChangeListener(foodListener);
+        prosciuttoCBX.setOnClickListener(baconListener);
+        cheeseRG.setOnCheckedChangeListener(foodListener);
+        sauceSBR.setOnSeekBarChangeListener(sauceListener);
+    }
+
+
 }
